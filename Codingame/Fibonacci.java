6a1efb4e39f272d7a07056b2c1fb682318ad9391
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-08 23:19:42 +0300

    added fibonacci algorithm

---
+++ Codingame/Fibonacci.java
@@ -0,0 +1,17 @@
+package Codingame;
+
+class Fibonacci {
+    public static void fibonacci() {
+        int a = 0;
+        int b = 1;
+        int c;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i = 0; i <= 10; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(c);
+        }
+    }
+}
